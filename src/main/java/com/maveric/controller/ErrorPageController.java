5695e0825f6b8a16fc2e906d49e0218a971f8741
Ram Krishnan <ramkrishnane@maveric-systems.com>
2022-07-25 12:50:25 +0530

    Added 404 Error Page in ErrorController

--- src/main/java/com/maveric/controller/ErrorPageController.java
+++ src/main/java/com/maveric/controller/ErrorPageController.java
@@ -1,17 +1,16 @@
-//package com.maveric.controller;
-//
-//import org.springframework.boot.web.servlet.error.ErrorController;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//@Controller
-//public class ErrorPageController implements ErrorController{
-//    private final static String PATH = "/error";
-//    @RequestMapping(PATH)
-//    @ResponseBody
-//    public String getErrorPath() {
-//        // TODO Auto-generated method stub
-//        return "No Mapping Found";
-//    }
-//
-//}
+package com.maveric.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
+public class ErrorPageController implements ErrorController{
+    private final static String PATH = "/error";
+    @RequestMapping(PATH)
+    public String handleError() {
+        //do something like logging
+        return "error";
+    }
+
+}
